46eb324d8380de38dd30f64247f09afa207a3deb
yoshimura <osaka.chiba2005@gmail.com>
2021-02-09 09:07:14 +0000

    add column7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,6 @@ class TasksController < ApplicationController
     end
     
     def show
-        set_task
     end
     
     def new
@@ -25,12 +24,10 @@ class TasksController < ApplicationController
     end
  
     def edit
-        set_task
     end
     
     def update
-        set_task
-        
+
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
             redirect_to @task
@@ -41,7 +38,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        set_task
         @task.destroy
 
         flash[:success] = 'Task は正常に削除されました'
