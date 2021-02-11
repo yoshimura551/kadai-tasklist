46001ae864ca5b2555d062fbf50deeaf0982d280
yoshimura <osaka.chiba2005@gmail.com>
2021-02-11 12:36:37 +0000

    for heroku4

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_02_07_090515) do
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
